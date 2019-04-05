e504936d281548ddcdafaa5e0522535e3c1bd5df
cktamucalgary <cktam@ucalgary.ca>
2019-04-04 22:41:49 -0600

    Submitting the form to log in works, atm just returns a success message and ID

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
 </ul>
 <h2>Please enter your login information</h2>
 <div class="boxDiv">
-<form action="/loginAuth.html" method="POST">
+<form action="login.php" method="POST">
   Username or Email <br><input type="text" name = "username" placeholder="username or email" required/> </br><br>
   Password <br><input type="password" name = "password" placeholder="password" required/></br><br>
   Select your login mode <br>
@@ -22,6 +22,31 @@
   <input type="submit" value = Login />
 </form>
 </div>
+
+<?php
+$form_username = $_POST['username'];
+$form_password = $_POST['password'];
+
+$mysqli = new mysqli("127.0.0.1", "root", "root", "Auth");
+  if ($mysqli->connect_errno) {
+    echo "Connected Successfully!";
+  }
+  
+  $stmt = $mysqli->prepare("SELECT personID FROM Login WHERE email=? AND password=?");
+  $stmt->bind_param("ss",$form_username,$form_password);
+  $stmt->execute();
+  $stmt->store_result();
+
+  if ($stmt->num_rows() == 1) {
+    $stmt->bind_result($personID);
+    $stmt->fetch();
+    echo '{"Success": true, "personID":' . $personID . '}';
+  } else {
+    echo '{"Success": false, "personID_id": null}';
+  }
+  $stmt->close();
+  
+?>
 
 </body>
 </html>
