9a97134ff745bc7fec69738748894a7523ba0394
Joey T <cktam@ucalgary.ca>
2019-04-08 15:47:39 -0600

    Init entries into db

--- initdb.sql
+++ initdb.sql
@@ -16,6 +16,9 @@ CREATE TABLE CPSC471.Person (
 INSERT INTO CPSC471.Person (firstName, lastName, phone, passportNo) VALUES ('Joan','Tam','123456789','HD123');
 INSERT INTO CPSC471.Person (firstName, lastName, phone, passportNo) VALUES ('Umer','Hassan','123456780','HD456');
 INSERT INTO CPSC471.Person (firstName, lastName, phone, passportNo) VALUES ('Cole','Parker','23456789','HD789');
+INSERT INTO CPSC471.Person (firstName, lastName, phone, passportNo) VALUES ('Bob','Bobby','987654321','HD111');
+INSERT INTO CPSC471.Person (firstName, lastName, phone, passportNo) VALUES ('Rob','Robby','098765432','HD222');
+INSERT INTO CPSC471.Person (firstName, lastName, phone, passportNo) VALUES ('Todd','Toddy','9876543','HD333');
 
 DROP TABLE IF EXISTS CPSC471.Admin;
 CREATE TABLE CPSC471.Admin (
@@ -42,6 +45,10 @@ CREATE TABLE CPSC471.Member (
 	UNIQUE(email)
 ) ENGINE=INNODB;
 
+INSERT INTO CPSC471.Member (personID,email, password) VALUES ('4','test@email.com','pw');
+INSERT INTO CPSC471.Member (personID,email, password) VALUES ('5','email@test.com','pw');
+INSERT INTO CPSC471.Member (personID,email, password) VALUES ('6','hi@emails.com','pw');
+
 DROP TABLE IF EXISTS CPSC471.Guest;
 CREATE TABLE CPSC471.Guest (
 	personID INT NOT NULL,
@@ -58,6 +65,10 @@ CREATE TABLE CPSC471.Plane (
 	PRIMARY KEY(serialNo)
 ) ENGINE=INNODB;
 
+INSERT INTO CPSC471.Plane(serialNo, NoOfSeats, company, planeType) VALUES ('ABC123',525,'Airbus','A380');
+INSERT INTO CPSC471.Plane(serialNo, NoOfSeats, company, planeType) VALUES ('DEC456',200, 'Boeing','777X');
+INSERT INTO CPSC471.Plane(serialNo, NoOfSeats, company, planeType) VALUES ('GHI789',375,'Bombardier','Q400');
+
 DROP TABLE IF EXISTS CPSC471.Airport;
 CREATE TABLE CPSC471.Airport (
 	airportCode VARCHAR(255),
@@ -66,21 +77,28 @@ CREATE TABLE CPSC471.Airport (
 	PRIMARY KEY(airportCode)
 ) ENGINE=INNODB;
 
+INSERT INTO CPSC471.Airport(airportCode,city,airportName) VALUES ('YYC','Calgary','Calgary International Airport');
+INSERT INTO CPSC471.Airport(airportCode,city,airportName) VALUES ('YVR','Vancouver','Vancouver International Airport');
+INSERT INTO CPSC471.Airport(airportCode,city,airportName) VALUES ('NRT','Tokyo','Narita International Airport');
+
 DROP TABLE IF EXISTS CPSC471.Terminal;
 CREATE TABLE CPSC471.Terminal (
-	terminalCode VARCHAR(255),
-	terminalName VARCHAR(255) NOT NULL,
-	Gates VARCHAR(255) NOT NULL,
+	airportCode VARCHAR(255),
+	Gate VARCHAR(255) NOT NULL,
 	terminalType VARCHAR(255) NOT NULL,
 	terminalStatus VARCHAR(255) NOT NULL,
-	PRIMARY KEY(terminalName),
-	FOREIGN KEY (terminalCode) REFERENCES Airport(airportCode)
+	PRIMARY KEY(Gate),
+	FOREIGN KEY (airportCode) REFERENCES Airport(airportCode)
 ) ENGINE=INNODB;
+
+INSERT INTO CPSC471.Terminal(airportCode,Gate,terminalType,terminalStatus) VALUES ('YYC','C10','Domestic','Boarding');
+INSERT INTO CPSC471.Terminal(airportCode,Gate,terminalType,terminalStatus) VALUES ('YVR','D58','International','Arrived');
+INSERT INTO CPSC471.Terminal(airportCode,Gate,terminalType,terminalStatus) VALUES ('NRT','J77','International','Departed');
 
 DROP TABLE IF EXISTS CPSC471.Flight;
 CREATE TABLE CPSC471.Flight (
     flightNo INT AUTO_INCREMENT,
-    flightDate DATETIME NOT NULL,
+    flightDate DATE NOT NULL,
     adminID INT NOT NULL,
 	duration VARCHAR(255) NOT NULL,
 	DAirportCode VARCHAR(10) NOT NULL,
@@ -94,6 +112,10 @@ CREATE TABLE CPSC471.Flight (
 	FOREIGN KEY (AAirportCode) REFERENCES Airport(airportCode)
 ) ENGINE=INNODB;
 
+INSERT INTO CPSC471.Flight(flightDate,adminID,duration,DAirportcode,AAirportCode,scheduledAtime,planeNo,planeType) VALUES ('2019-04-08','1','1h 19m','YYC','YVR','08:30','ABC123','A380');
+INSERT INTO CPSC471.Flight(flightDate,adminID,duration,DAirportcode,AAirportCode,scheduledAtime,planeNo,planeType) VALUES ('2019-04-09','2','9h 12m','YVR','NRT','13:25','DEF456','A380');
+INSERT INTO CPSC471.Flight(flightDate,adminID,duration,DAirportcode,AAirportCode,scheduledAtime,planeNo,planeType) VALUES ('2019-04-05','3','10h 45m','YYC','NRT','17:05','GHI789','777X');
+
 DROP TABLE IF EXISTS CPSC471.Booking;
 CREATE TABLE CPSC471.Booking (
 	personID INT NOT NULL,
@@ -101,11 +123,15 @@ CREATE TABLE CPSC471.Booking (
 	seatColumn INT NOT NULL,
 	gateNo VARCHAR(255) NOT NULL,
 	flightNo INT NOT NULL,
-	flightDate DATETIME NOT NULL,
+	flightDate DATE NOT NULL,
 	PRIMARY KEY (seatRow, seatColumn),
 	FOREIGN KEY (personID) REFERENCES Member(personID),
 	FOREIGN KEY (flightNo,flightDate) REFERENCES Flight(flightNo,flightDate)
 ) ENGINE=INNODB;
+
+INSERT INTO CPSC471.Booking(personID,seatRow,seatColumn,gateNo,flightNo,flightDate) VALUES ('4','5','1','D58','1','2019-04-08');
+INSERT INTO CPSC471.Booking(personID,seatRow,seatColumn,gateNo,flightNo,flightDate) VALUES ('5','28','3','C10','2','2019-04-09');
+INSERT INTO CPSC471.Booking(personID,seatRow,seatColumn,gateNo,flightNo,flightDate) VALUES ('6','12','2','J77','3','2019-04-05');
 
 DROP TABLE IF EXISTS CPSC471.Baggage;
 CREATE TABLE CPSC471.Baggage (
@@ -116,20 +142,16 @@ CREATE TABLE CPSC471.Baggage (
 	bagLength DOUBLE NOT NULL,
 	bagHeight DOUBLE NOT NULL,
 	bagWidth DOUBLE NOT NULL,
-	badWeight DOUBLE NOT NULL,
+	bagWeight DOUBLE NOT NULL,
 	travelClass VARCHAR(255) NOT NULL,	
 	PRIMARY KEY (tagNo),
 	FOREIGN KEY (flightNo) REFERENCES Flight(flightNo),
 	FOREIGN KEY (seatRow,seatColumn) REFERENCES Booking(seatRow,seatColumn)
 ) ENGINE=INNODB;
 
-DROP TABLE IF EXISTS CPSC471.CreditCard;
-CREATE TABLE CPSC471.CreditCard (
-	personID INT,
-	cardNumber INT NOT NULL,
-	expiryDate VARCHAR(255) NOT NULL,
-	FOREIGN KEY (personID) REFERENCES Member(personID)
-) ENGINE=INNODB;
+INSERT INTO CPSC471.Baggage(seatRow,seatColumn,flightNo,bagLength,bagHeight,bagWidth,bagWeight,travelClass) VALUES ('5','1','1','10','10','10','20.998','Economy');
+INSERT INTO CPSC471.Baggage(seatRow,seatColumn,flightNo,bagLength,bagHeight,bagWidth,bagWeight,travelClass) VALUES ('28','3','2','20','20','20','30.034','Business');
+INSERT INTO CPSC471.Baggage(seatRow,seatColumn,flightNo,bagLength,bagHeight,bagWidth,bagWeight,travelClass) VALUES ('12','2','3','30','10','15','15.789','Premium');
 
 DROP TABLE IF EXISTS CPSC471.Fare;
 CREATE TABLE CPSC471.Fare (
@@ -137,10 +159,26 @@ CREATE TABLE CPSC471.Fare (
 	seatRow INT NOT NULL,
 	seatColumn INT NOT NULL,
 	flightNo INT NOT NULL,
-	flightDate DATETIME NOT NULL,
+	flightDate DATE NOT NULL,
 	tax DOUBLE NOT NULL,
 	price DOUBLE NOT NULL,
 	FOREIGN KEY (personID) REFERENCES Member(personID),
 	FOREIGN KEY (seatRow,seatColumn) REFERENCES Booking(seatRow,seatColumn),
 	FOREIGN KEY (flightNo,flightDate) REFERENCES Flight(flightNo,flightDate)
 ) ENGINE=INNODB;
+
+INSERT INTO CPSC471.Fare(personID,seatRow,seatColumn,flightNo,flightdate,tax,price) VALUES ('4','5','1','1','2019-04-08','26.345','200.89');
+INSERT INTO CPSC471.Fare(personID,seatRow,seatColumn,flightNo,flightdate,tax,price) VALUES ('5','28','3','2','2019-04-09','90.23','800.97');
+INSERT INTO CPSC471.Fare(personID,seatRow,seatColumn,flightNo,flightdate,tax,price) VALUES ('6','12','2','3','2019-04-05','100.03','1005.81');
+
+DROP TABLE IF EXISTS CPSC471.CreditCard;
+CREATE TABLE CPSC471.CreditCard (
+	personID INT,
+	cardNumber INT NOT NULL,
+	expiryDate DATE NOT NULL,
+	FOREIGN KEY (personID) REFERENCES Member(personID)
+) ENGINE=INNODB;
+
+INSERT INTO CPSC471.CreditCard(personID,cardNumber,expiryDate) VALUES ('4','1234','2020-04-08');
+INSERT INTO CPSC471.CreditCard(personID,cardNumber,expiryDate) VALUES ('5','45983495','2021-04-09');
+INSERT INTO CPSC471.CreditCard(personID,cardNumber,expiryDate) VALUES ('6','3904832904','2022-04-10');
