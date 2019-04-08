09a8f46e9708ced05b15c875ccf9e1f05f8305fb
DESKTOP-SUSP09S\colep <cole.parker@hotmail.ca>
2019-04-08 12:10:33 -0600

    contextual header

--- includes/header.php
+++ includes/header.php
@@ -4,11 +4,19 @@
 </head>
 <body>
 <ul>
-  <li><a class="active" href="main.php">Home</a></li>
-  <li><a href="searchflight.php">Search Flights</a></li>
-  <li><a href="bookflight.php">Book Flights</a></li>
-  <li><a href="manageflight.php">Manage Flights</a></li>
-  <li style="float:right"><a href="logout.php">Logout</a></li>
-  <li style="float:right"><a href="login.php">Login</a></li>
-  <li style="float:right"><a href="signup.php">Sign Up</a></li>
+<?php
+  session_start();
+  echo "<li><a class=\"active\" href=\"main.php\">Home</a></li>";
+  echo "<li><a href=\"searchflight.php\">Search Flights</a></li>";
+  echo "<li><a href=\"bookflight.php\">Book Flights</a></li>";
+  if (isset($_SESSION["sesIsAdmin"]) && $_SESSION["sesIsAdmin"] == 1) {
+    echo "<li><a href=\"manageflight.php\">Manage Flights</a></li>";
+  }
+  if (isset($_SESSION["sesPersonID"])) {
+    echo "<li style=\"float:right\"><a href=\"logout.php\">Logout</a></li>";
+  } else {
+    echo "<li style=\"float:right\"><a href=\"login.php\">Login</a></li>";
+    echo "<li style=\"float:right\"><a href=\"signup.php\">Sign Up</a></li>";
+  }
+?>
 </ul>
--- signup.php
+++ signup.php
@@ -7,29 +7,35 @@
 </head>
 <h2>Sign up</h2>
 <div class="boxDiv">
-<form action="signup.php" method="POST">
-  Email <br><input type="text" name = "username" placeholder="Email" required/> </br><br>
-  Password <br><input type="password" name = "password" placeholder="Password" required/></br><br>
-  <input type="submit" value = "Sign up" />
-</form>
+
 </div>
 
 <?php
 session_start();
-$form_username = $_POST['username'];
-$form_password = $_POST['password'];
-
-//This is terrible code but its basically server/username/password/db name
-$mysqli = new mysqli("127.0.0.1", "root", "root", "CPSC471");
-  if ($mysqli->connect_errno) {
-    echo "Connected Successfully!";
+if (isset($_SESSION["sesPersonID"])) {
+  echo "Cannot signed up when logged in.";
+} else {
+  echo "<form action=\"signup.php\" method=\"POST\">";
+  echo "Email <br><input type=\"text\" name = \"username\" placeholder=\"Email\" required/> </br><br>";
+  echo "Password <br><input type=\"password\" name = \"password\" placeholder=\"Password\" required/></br><br>";
+  echo "<input type=\"submit\" value = \"Sign up\" />";
+  echo "</form>";
+  $form_username = $_POST['username'];
+  $form_password = $_POST['password'];
+  
+  //This is terrible code but its basically server/username/password/db name
+  $mysqli = new mysqli("127.0.0.1", "root", "root", "CPSC471");
+    if ($mysqli->connect_errno) {
+      echo "Connected Successfully!";
+    }
+  
+    $stmt = $mysqli->prepare("INSERT INTO Login(email,password,isAdmin) VALUES (?,?,0)");
+    $stmt->bind_param("ss",$form_username,$form_password);
+    $stmt->execute();
+  
+    $stmt->close();
   }
 
-  $stmt = $mysqli->prepare("INSERT INTO Login(email,password,isAdmin) VALUES (?,?,0)");
-  $stmt->bind_param("ss",$form_username,$form_password);
-  $stmt->execute();
-
-  $stmt->close();
 
 ?>
 
