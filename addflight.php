596a235e54bd996f0a4cc07f51f618b10181b842
Muhammad Hassan <33638819+umerhassan@users.noreply.github.com>
2019-04-10 17:34:16 -0700

    fixed addflight

--- addflight.php
+++ addflight.php
@@ -2,11 +2,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Add a Flight</title>
+  <title>Add Flights</title>
   <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
-<h1>Add a Flight</h1>
-</h2> 
+<h1>Add flights</h1>
 <div class="boxDiv">
 <?php
 if(!isset($_SESSION))
@@ -14,43 +13,46 @@ if(!isset($_SESSION))
        session_start();
    }
    if (isset($_SESSION["sesIsAdmin"])) {
-    echo "<form action=\"addflight.php\" method=\"POST\">";
-    echo "Flight Date <br><input type=\"date\" name = \"flightdate\" required/></br><br>";
-    echo "Expected Duration <br><input type=\"text\" name = \"flightduration\" required/></br><br>";
-    echo "Departure Airport <br><input type=\"text\" name = \"departureairport\" required/></br><br>";
-    echo "Arrival Airport <br><input type=\"text\" name = \"arrivalairport\" required/></br><br>";
-    echo "Scheduled Departure Time <br><input type=\"time\" name = \"departuretime\" required/></br><br>";
-    echo "Scheduled Arrival Time <br><input type=\"time\" name = \"arrivaltime\" required/></br><br>";
-    echo "Plane Number <br><input type=\"text\" name = \"planenumber\" required/></br><br>";
-    echo "Plane Type <br><input type=\"text\" name = \"planetype\" required/></br><br>";
-    echo "<input type=\"submit\" value = \"Add Flight\" />";
-    if(isset($_POST['flightdate']) && isset($_POST['flightduration']) && isset($_POST['departureairport']) 
-    && isset($_POST['arrivalairport']) && isset($_POST['departuretime']) && isset($_POST['arrivaltime']) && isset($_POST['planenumber'])
-    && isset($_POST['planetype'])) {
-      $stmt = $mysqli->prepare("INSERT INTO Flight(flightDate,adminID,duration,DAirportcode,AAirportCode,scheduledDtime,scheduledAtime,planeNo,planeType) VALUES (?,?,?,?,?,?,?,?,?)");
-      $stmt->bind_param("sisssssss", $_POST['flightdate'], $_SESSION['sesPersonID'], $_POST['flightduration'], $_POST['departureairport'], $_POST['arrivalairport'], $_POST['departuretime'], $_POST['arrivaltime'], $_POST['planenumber'], $_POST['planetype']);
-      $stmt->execute();
-  
-      echo "Success! You have added this flight.";
-      header("Location:main.php", true, 301);
-      $stmt->close();
-  
-  }
-?>
-<?php }
-else {
-  header("Location:main.php", true, 301);
-}
-
 ?>
 
-
-
-  
+<form action="addflight.php" method="POST">
+  Flight Date <br><input type="date" name = "flightdate" required/></br><br>
+  Expected Duration <br><input type="text" name = "duration" required/></br><br>
+  Departure Airport <br><input type="text" name = "da" required/></br><br>
+  Arrival Airport <br><input type="text" name = "aa" required/></br><br>
+  Scheduled Departure time <br><input type="time" name = "dt" required/></br><br>
+  Scheduled Arrival time <br><input type="time" name = "at" required/></br><br>
+  Plane No <br><input type="text" name = "planeNo" required/></br><br>
+  Plane Type <br><input type="text" name = "planeType" required/></br><br>
+  <input type="submit" name="addButton" value = "Add Flight" />
 </form>
 </div>
+<?php }
+else {
+  echo "Only Admins can add flights!";
+}
+?>
 
-</body>
+<?php
+  if(isset($_POST['flightdate']) && isset($_POST['duration']) && isset($_POST['da']) && isset($_POST['aa']) && isset($_POST['dt']) && isset($_POST['at']) && isset($_POST['planeNo']) && isset($_POST['planeType'])) {
+    $duration = $_POST['duration'];
+    $date = $_POST['flightdate'];
+    $da = $_POST['da'];
+    $aa = $_POST['aa'];
+    $dt = $_POST['dt'];
+    $at = $_POST['at'];
+    $planeNo = $_POST['planeNo'];
+    $planeType = $_POST['planeType'];
+    //echo $duration.$date.$da.$aa.$dt.$at.$planeNo.$planeType;
 
+    $mysqli = new mysqli("127.0.0.1", "root", "root", "CPSC471");
+    if ($mysqli->connect_errno) {
+      echo "Connection Failed!";
+    }
 
-</html>
+    $sql = "INSERT INTO CPSC471.Flight(flightDate,adminID,duration,DAirportcode,AAirportCode,scheduledDtime,scheduledAtime,planeNo,planeType) VALUES ('$date','1','$duration','$da','$aa','$dt','$at','$planeNo','$planeType');";
+    if(mysqli_query($mysqli, $sql)){
+    echo "Records added successfully.";
+    } 
+  }
+?>
